65f8f9015bac8d8381754f941d697fcb07f8e4ee
bhrq12 <49489405+bhrq12@users.noreply.github.com>
2025-05-16 16:08:05 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -45,9 +45,9 @@ git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plu
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+svn export https://github.com/xiaorouji/openwrt-passwall/tree/main/luci-app-passwall package/luci-app-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall2/tree/main/luci-app-passwall2 package/luci-app-passwall2
+svn export https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/luci-app-openclash
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
